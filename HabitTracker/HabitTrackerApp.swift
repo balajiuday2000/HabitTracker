9f1e794ad67492d99aa6c13ab38af02dcb1e252b
Balaji Udayakumar <balaji@bu.edu>
2026-08-25 09:28:36 -0400

    Initial commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Software Merchant on 8/25/26.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
