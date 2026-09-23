0e512dac2d588058246491aa089484729a075de8
Balaji Udayakumar <balaji@bu.edu>
2026-09-23 15:40:55 -0400

    Clean up

--- HabitTracker/HabitsListView.swift
+++ HabitTracker/HabitsListView.swift
@@ -61,7 +61,6 @@ struct HabitsListView: View {
                         }
                     }
                 }
-                
             }
             .navigationTitle("Habits")
             .toolbar {
